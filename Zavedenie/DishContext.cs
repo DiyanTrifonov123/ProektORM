4e60f72be6fc302878636deceaa7143e3c16e25d
NikolayFitfov <nikolay.fitfov649@edu1.pgzaimov.com>
2025-06-05 10:21:24 +0300

    First commit

---
+++ Zavedenie/DishContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zavedenie
+{
+    public class DishContext:DbContext
+    {
+        public DishContext():base("DishContext")
+        {
+            
+        }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<DishType> DishesType { get; set; }
+    }
+}
